243c06cd495198bf7cc542ea005288db79aba7ea
richeerichee <richiejobfair@yahoo.in>
2025-02-21 07:47:44 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -3,5 +3,8 @@ public class helloworld {
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
 	System.out.println ("Hello World!");
+	    System.out.println ("hi Richard");
+	    System.out.println ("welcome to the java");
+	    System.out.println ("you have complied with git");
     }
 }
